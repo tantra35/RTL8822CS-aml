113964b5adef1aee8036e8ada9f465be4d679ff6
tantra35 <ruslan.usifov@gmail.com>
2020-09-24 02:00:46 +0300

    fix

--- core/rtw_xmit.c
+++ core/rtw_xmit.c
@@ -16,6 +16,7 @@
 
 #include <drv_types.h>
 #include <hal_data.h>
+#include <uapi/linux/sched/types.h>
 
 static u8 P802_1H_OUI[P80211_OUI_LEN] = { 0x00, 0x00, 0xf8 };
 static u8 RFC1042_OUI[P80211_OUI_LEN] = { 0x00, 0x00, 0x00 };
